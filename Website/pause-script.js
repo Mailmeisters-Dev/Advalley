2544e9d53fc6db6f5a0f268fd503045b9c3153f0
Sybren <hallo@mailmeisters.nl>
2022-03-09 15:40:58 +0100

    Final version

--- Website/pause-script.js
+++ Website/pause-script.js
@@ -1,18 +1,88 @@
-var frequencyRadios = document.querySelectorAll('[name="dbfield50"]');
+var InitialFrequencyValue = document.getElementById('js-Initial-frequency-hidden').value;
 
+// Retrieve frequency and pause radios
+var frequencyRadios = document.querySelectorAll('input[name="dbfield178"]');
+var pauseRadios = document.querySelectorAll('input[name="dbfield177"]');
+
+// Retrieve button to change innerText later
+var submitButton = document.querySelector('.submit__button');
+
+// Create array from frequencyRadios so we can get the specific, needed radio for functions
 var frequencyRadiosArray = Array.from(frequencyRadios);
 
-var openPauseElement = frequencyRadiosArray[frequencyRadiosArray.length -1];
+// This is 'Geen' radio
+var openPauseElement = frequencyRadiosArray[frequencyRadiosArray.length - 1];
+
+// All radio's but 'Geen'
 var closePauseElement = frequencyRadiosArray.slice(0, -1);
 
+// The pause element to show or hide
 var pauseElement = document.querySelector('.js-pause-container');
 
-frequencyRadios.forEach(function (radio, index) {
-  radio.addEventListener('change', function() {
-    if (frequencyRadios[index] == openPauseElement) {
-      pauseElement.setAttribute('class', 'row p-b-32 js-pause-container');
-    } else {
-      pauseElement.setAttribute('class', 'row p-b-32 js-pause-container hide');
-    }
+// Called from clicking frequency radios
+function hideShowPauseElement(action) {
+  if (action === 'hide') {
+    pauseElement.setAttribute('class', 'row p-b-32 js-pause-container hide');
+  } else {
+    pauseElement.setAttribute('class', 'row p-b-32 js-pause-container');
   }
 }
+
+function resetPauseRadios() {
+  pauseRadios.forEach(function(radio) {
+    radio.checked = false;
+  });
+};
+
+function resetFrequencyRadios() {
+  frequencyRadios.forEach(function(radio) {
+    radio.checked = false;
+  });
+}
+
+// We created a dummy, hidden checkbox with the Initial frequency which is checked when customer selects a temporary pause. When customer then selects a frequency radio we uncheck this
+// Retrieve the hidden radio
+var hiddenInitialFrequencyRadio = frequencyRadiosArray[0];
+
+function checkInitialFrequency() {
+  hiddenInitialFrequencyRadio.checked = true;
+}
+
+function uncheckInitialFrequency() {
+  hiddenInitialFrequencyRadio.checked = false;
+}
+
+// Call function to hide or show pause element based on which radio is clicked
+frequencyRadios.forEach(function(radio, index) {
+  radio.addEventListener('click', function() {
+    if (frequencyRadios[index] == openPauseElement) {
+      hideShowPauseElement('show');
+    } else {
+      hideShowPauseElement('hide');
+    }
+    // If customer clicks any frequency radio: uncheck all pause radios
+    resetPauseRadios();
+    // If customer clicks any frequency radio: uncheck the Initial frequency radio
+    uncheckInitialFrequency();
+    submitButton.value = 'Afmelden voor nieuwsbrief';
+  })
+});
+
+// If customer clicks any pause radio: change button text, uncheck all frequency radios and check the Initital frequency radio
+pauseRadios.forEach(function(radio) {
+  radio.addEventListener('click', function() {
+    submitButton.value = 'Tijdelijke pauze bevestigen';
+    resetFrequencyRadios();
+    checkInitialFrequency();
+  });
+});
+
+// If customer clicks reset pause text under the radios: change button text, set frequency to 'Dagelijks', uncheck all pause radios and uncheck the Initial frequency radio
+var resetPause = document.querySelector('.js-pause-reset');
+resetPause.addEventListener('click', function() {
+  submitButton.value = 'Afmelden voor nieuwsbrief';
+  frequencyRadiosArray[0].checked = true;
+  hideShowPauseElement('hide');
+  resetPauseRadios();
+  uncheckInitialFrequency();
+});
